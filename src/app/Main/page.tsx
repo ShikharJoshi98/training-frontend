a56be95e2517ba6e71644669fd6aaa630263dc35
ShikharJoshi98 <shikharjoshi89@gmail.com>
2025-07-17 03:58:06 +0530

    header and navbar

--- src/app/Main/page.tsx
+++ src/app/Main/page.tsx
@@ -1,10 +1,23 @@
+'use client'
 import Navbar from '@/components/Navbar'
+import Image from 'next/image'
 import React from 'react'
+import { FaAward } from 'react-icons/fa'
+import { FaArrowRightLong } from 'react-icons/fa6'
 
 export default function Main() {
     return (
         <>
             <Navbar />
+            <section className='px-4 py-8 relative sm:py-12 bg-gradient-to-br from-[#e5e5e5] via-[#ccd4df] to-[#92aee2]'>
+                <div className='flex flex-col items-center gap-4 sm:gap-6'>
+                    <p className='flex items-center font-semibold gap-2 py-1 px-4 bg-white/10 text-[#f86f03] border rounded-4xl w-fit text-[10px] whitespace-nowrap sm:text-sm'><FaAward />STEP INTO THE IT WORLD WITH SNT</p>
+                    <h1 style={{ fontFamily: "Urbanist-bold" }} className='text-2xl leading-tight mt-2 text-zinc-800 text-center sm:text-4xl lg:text-[56px]'>Join the  No. 1 Institute with <br /><span style={{ fontFamily: "Urbanist-bold" }} className='text-[#525fe1]'>100% Job Placement Support.</span></h1>
+                    <p className='text-center block font-semibold text-gray-700 sm:hidden max-w-[325px] text-xs md:text-lg'>Kickstart your IT career with job-ready training and 100% placement support from SNT.</p>
+                    <p className='text-center font-semibold text-gray-700 hidden sm:block sm:max-w-[525px] lg:max-w-[800px] text-[15px] md:text-lg'>Achieve your IT career goals with job-ready training and 100% job assistance. SNT ensures you master essential IT skills to succeed in today&apos;s competitive world.</p>
+                    <button className='py-2 pl-4 font-semibold text-sm sm:text-base cursor-pointer pr-2 text-white bg-[#525fe1] rounded-full flex items-center gap-2 mt-2'>Get Started Now <span className='bg-white text-black rounded-full p-2'><FaArrowRightLong /></span></button>
+                </div>
+            </section >
         </>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { CiPhone } from "react-icons/ci";
 import { FaFacebook, FaInstagram, FaLinkedin, FaPhoneAlt, FaTwitter, FaYoutube } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { LuMessageSquare } from "react-icons/lu";
@@ -9,42 +8,45 @@ import { RiMenuFill } from "react-icons/ri";
 export default function Navbar() {
     return (
         <>
-            <div className="bg-[#f86f03] p-1.5">
-                <p className="text-white px-4 leading-relaxed text-center text-wrap w-auto sm:w-[514px] md:w-[749px] lg:w-fit mx-auto">Job Oriented Courses for Everyone! Open to All Graduates, Diplomas, Long Years Gap, Passouts & Non-IT Fields. <span className="text-[#231F40] font-semibold text-xl">Enquire Now 📩</span></p>
+            <div className="bg-[#f86f03] p-1">
+                <p className="text-white px-4 leading-relaxed text-center text-wrap w-auto sm:w-[514px] md:w-[749px] lg:w-fit mx-auto">Job Oriented Courses for Everyone! Open to All Graduates, Diplomas, Long Years Gap, Passouts & Non-IT Fields. <span className="text-[#231F40] font-bold text-lg">Enquire Now 📩</span></p>
             </div>
             <div className="bg-[#231F40]">
-                <ul className="list-none py-3 px-5 lg:px-10 max-w-6xl mx-auto hidden md:flex items-center justify-between">
+                <ul className="list-none py-2 px-5 max-w-6xl mx-auto hidden md:flex items-center justify-between">
                     <li className="flex text-white/70 items-center gap-4">
                         <div className="flex items-center gap-2">
                             <MdEmail className="w-4 text-[#f86f03]" />
                             <p className="cursor-pointer">Mnath.snt@gmail.com</p>
                         </div>
                         <div className="flex items-center gap-2">
-                            <FaPhoneAlt className="w-4 text-[#f86f03] cursor-pointer" />
+                            <FaPhoneAlt className="w-4 text-[#f86f03]" />
                             <p className="cursor-pointer">+91 8595161711</p>
                         </div>
                     </li>
-                    <li className="flex text-white items-center gap-4">Follow Us: <span className="flex items-center gap-2 text-white/70"><FaFacebook className="cursor-pointer hover:text-[#f86f03] duration-300" /><FaYoutube className="cursor-pointer hover:text-[#f86f03] duration-300" /><FaXTwitter className="cursor-pointer hover:text-[#f86f03] duration-300" /><FaInstagram className="cursor-pointer hover:text-[#f86f03] duration-300" /><FaLinkedin className="cursor-pointer hover:text-[#f86f03] duration-300" /><FaTwitter className="cursor-pointer hover:text-[#f86f03] duration-300" /></span></li>
+                    <li className="flex text-white items-center gap-4">Follow Us: <span className="flex items-center gap-3 text-white/70"><FaFacebook className="cursor-pointer hover:text-[#f86f03] duration-300" /><FaYoutube className="cursor-pointer hover:text-[#f86f03] duration-300" /><FaXTwitter className="cursor-pointer hover:text-[#f86f03] duration-300" /><FaInstagram className="cursor-pointer hover:text-[#f86f03] duration-300" /><FaLinkedin className="cursor-pointer hover:text-[#f86f03] duration-300" /><FaTwitter className="cursor-pointer hover:text-[#f86f03] duration-300" /></span></li>
                 </ul>
             </div>
-            <div className="p-3 sm:py-3 sm:px-5 lg:px-10 max-w-6xl mx-auto flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                    <Image src="/sntlogo.jpg" alt="SNT Logo" height={30} width={30} className="w-10 sm:w-16 rounded-md" />
-                    <h1 className="text-[#035283] text-3xl sm:text-4xl font-semibold">SNT</h1>
+            <div className="bg-white shadow-md">
+                <div className="p-3 sm:py-2 sm:px-5 max-w-6xl mx-auto flex items-center justify-between">
+                    <div className="flex items-center">
+                        <Image src="/sntlogo.jpg" alt="SNT Logo" height={60} width={64} className="w-10 h-14 object-cover sm:w-16 rounded-md" />
+                        <Image src="/sntname.png" alt="SNT Name" height={60} width={96} className="w-16 h-14 sm:w-24 object-cover cursor-pointer" />
+                    </div>
+                    <ul className="flex md:hidden list-none items-center text-[#231F40] gap-4">
+                        <li className="p-2 rounded-md shadow-md"><FaPhoneAlt className="size-4 sm:size-6 font-bold" /></li>
+                        <li className="p-2 rounded-md shadow-md"><LuMessageSquare className="size-4 sm:size-6" /></li>
+                        <li className="p-2 rounded-md shadow-md"><RiMenuFill className="size-4 sm:size-6" /></li>
+                    </ul>
+                    <ul className="lg:flex hidden list-none items-center gap-10">
+                        <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold">Home</li>
+                        <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold">Courses</li>
+                        <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold">Projects</li>
+                        <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold">Tutorial</li>
+                        <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold">About us</li>
+                        <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold">Blog</li>
+                    </ul>
+                    <span className="p-2 hidden md:block lg:hidden rounded-md shadow-md"><RiMenuFill className="size-4 sm:size-6" /></span>
                 </div>
-                <ul className="flex lg:hidden list-none items-center text-[#231F40] gap-2">
-                    <li className="p-2 rounded-md shadow-md"><CiPhone className="size-4 sm:size-6" /></li>
-                    <li className="p-2 rounded-md shadow-md"><LuMessageSquare className="size-4 sm:size-6" /></li>
-                    <li className="p-2 rounded-md shadow-md"><RiMenuFill className="size-4 sm:size-6" /></li>
-                </ul>
-                <ul className="lg:flex hidden list-none items-center gap-6">
-                    <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold text-lg">Home</li>
-                    <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold text-lg">Courses</li>
-                    <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold text-lg">Projects</li>
-                    <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold text-lg">Tutorial</li>
-                    <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold text-lg">About us</li>
-                    <li className="text-[#231F40] hover:text-blue-600 cursor-pointer duration-300 font-semibold text-lg">Blog</li>
-                </ul>
             </div>
         </>
     )
